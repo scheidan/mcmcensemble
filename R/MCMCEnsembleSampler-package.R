5715eb4731be6c9f60e24d8b2f1b0bdd4d5a5d5c
Hugo Gruson <hugo.gruson@protonmail.com>
2020-10-08 12:08:37 +0200

    Fix pkg documentation

---
+++ R/MCMCEnsembleSampler-package.R
@@ -0,0 +1,8 @@
+#' MCMCEnsembleSampler
+#'
+#' This package implements a particle Monte Carlo Markov Chain sampler
+#' with two different ways of creating proposals.
+#'
+#' @keywords internal
+"_PACKAGE"
+
